1ed2f73566c4054eb2c8b111c8cffcc155d5dcc7
Douglinhas <daguiaralcantara@gmail.com>
2022-12-02 01:26:01 -0300

    Resolução do desafio dos palíndromos de forma recursiva.

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if high_index == -1:
+        return False
+
+    if word[low_index] != word[high_index]:
+        return False
+    elif high_index > 1:
+        return is_palindrome_recursive(word[1:high_index], 0, high_index - 2)
+    else:
+        return True
