234eb2f558ece13f8a7092799515c961cd9532df
Douglinhas <daguiaralcantara@gmail.com>
2022-12-04 23:25:30 -0300

    Implementação do algoritmo de busca para o primeiro requisito.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    studing = 0
+    for student in permanence_period:
+        if type(student[0]) != int or type(student[1]) != int:
+            return None
+
+        interval = range(student[0], student[1] + 1)
+        if target_time in interval:
+            studing += 1
+                            
+    return studing
