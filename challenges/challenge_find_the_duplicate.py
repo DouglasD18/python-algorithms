a056a0e0ae3e63a4ec474072047e559fe5d0ea40
Douglinhas <daguiaralcantara@gmail.com>
2022-12-06 22:49:02 -0300

    Resolução do requisito 5.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,28 @@
+def iteration(nums):
+    duplicates = []
+    nums.sort()
+    for i, num in enumerate(nums):
+        numeric = type(num)
+        if numeric != int or num < 1:
+            return False
+        if i < len(nums) - 1 and num == nums[i + 1]:
+            duplicates.append(num)
+    return duplicates
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) < 2:
+        return False
+    duplicates = iteration(nums)
+    if type(duplicates) != list or len(duplicates) < 1:
+        return False
+    auxiliar = duplicates[0]
+    for num in duplicates:
+        if num != auxiliar:
+            return False
+    return auxiliar
+
+
+if __name__ == '__main__':
+    result = find_duplicate([1, 3, 4, 2, 2])
+    print(result)
